85ec9dae08cb0a1873ad751c8a0f7d4f0e289141
jl01128 <jackie01128@gmail.com>
2023-11-19 23:23:26 -0500

    Revert "removed all unecessary files besides log files"
    This reverts commit ac5300bdd45f003eccb8b7a2ee033137b9359437.

---
+++ h4/src/main/java/com/cis4615/app/App.java
@@ -0,0 +1,13 @@
+package com.cis4615.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
