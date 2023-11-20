587857ff46658313084010dcb73997f614dd2536
vboxuser <vboxuser@HW4.myguest.virtualbox.org>
2023-11-19 22:33:53 -0500

    final testings

--- installJava.sh
+++
@@ -1,4 +0,0 @@
-# Install Jave runtime (jre)
-sudo apt install default-jre |tee install.log
-# Install Jave Developer Kit (jdk)
-sudo apt install default-jdk |tee -a install.log
--- maven.sh
+++
@@ -1,5 +0,0 @@
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-export M2_HOME=/opt/apache-maven
-export MAVEN_HOME=/opt/apache-maven
-export PATH=${M2_HOME}/bin:${PATH}
-
